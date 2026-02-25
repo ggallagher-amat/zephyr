acff6aa849060c840890428a91b28b61a28513ba
Greg Gallagher <Greg_Gallagher@amat.com>
2026-02-24 17:12:37 -0800

    E7_DK: Add the RTSS side of the e7 dk
    This can be dropped when alif brings this in.  This is for testing the
    BT murata 2EL module.
    
    Signed-off-by: Greg Gallagher <Greg_Gallagher@amat.com>

---
+++ boards/alif/ensemble_e7_dk/board.cmake
@@ -0,0 +1,12 @@
+# Copyright (c) 2025 Alif Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_SOC_AE722F80F55D5XX_RTSS_HP)
+	board_runner_args(jlink "--device=AE722F80F55D5_M55_HP" "--speed=4000")
+endif()
+
+if(CONFIG_SOC_AE722F80F55D5XX_RTSS_HE)
+	board_runner_args(jlink "--device=AE722F80F55D5_M55_HE" "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
