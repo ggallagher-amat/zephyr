e454dfe03e6a64055921494e4203b373c2ef85ef
Greg Gallagher <Greg_Gallagher@amat.com>
2026-04-14 21:41:07 -0400

    ALIF: E7: Add support for Cortex A32 core
    Add support for apss core
    
    Signed-off-by: Greg Gallagher <Greg_Gallagher@amat.com>

--- soc/alif/ensemble/common/soc.h
+++ soc/alif/ensemble/common/soc.h
@@ -6,6 +6,10 @@
 #ifndef _SOC_ALIF_ENSEMBLE_SOC_H_
 #define _SOC_ALIF_ENSEMBLE_SOC_H_
 
+#if defined(SOC_AE722F80F55D5LS_RTSS_HP) || defined(SOC_AE722F80F55D5LS_RTSS_HE)
+
 #include <cmsis_core_m_defaults.h>
+
+#endif
 
 #endif /* _SOC_ALIF_ENSEMBLE_SOC_H_ */
